b3dd44b95e674bedee3c2416616f925240324fa7
Rahul Rameshwar Rathod <120775539+assistantsiri97@users.noreply.github.com>
2023-07-20 13:08:33 +0530

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,34 @@
+import instaloader
+from pytube import YouTube
+def audio_from_youtube(url):
+    yt=YouTube(url)
+    so=yt.streams.filter(only_audio=True).first()
+
+    so.download()
+
+
+
+def instagram():
+    insta = instaloader.Instaloader()
+    user = input("Enter your instagram username: ")
+    insta.download_profile(user, profile_pic_only=True)
+
+    profile = instaloader.Profile.from_username(insta.context, user)
+    posts = list(profile.get_posts())
+    latestPost = posts[0]
+    insta.download_post(latestPost, target=profile.username)
+
+
+choice = input("Enter 1 for instagram operation and 2 for youtube: ")
+
+if choice == '1':
+    instagram()
+else:
+
+    video_url = input("Enter youtube url")
+    audio_from_youtube(video_url)
+
+
+
+
+
